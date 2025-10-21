2353dd1ef10a7bcc0f9a0db6c2165f85445c060e
EVO <bassemgamalosman12@gmail.com>
2025-10-21 17:41:10 +0300

    setup dependency injection with GetIt; register ArticleRepository, FetchArticlesUseCase, and HomeScreenBloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_developer_assessment/data/repository/articles_repository.dart';
+import 'package:flutter_developer_assessment/data/transformer/article_transformer.dart';
+import 'package:flutter_developer_assessment/domain/usecase/fetch_articles_usecase.dart';
 import 'package:flutter_developer_assessment/ui/screen/home_screen/bloc/home_screen_bloc.dart';
 import 'package:flutter_developer_assessment/ui/screen/home_screen/home_screen.dart';
+import 'package:get_it/get_it.dart';
 
-import 'data/api/configs_api.dart';
+import 'data/api/article_remote_datasource.dart';
 
 void main() {
+  _setupDependencies();
+
   runApp(const ArticleApp());
 }
 class ArticleApp extends StatelessWidget {
@@ -18,9 +24,23 @@ class ArticleApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: BlocProvider(
-          create: (context) => HomeScreenBloc(ConfigService()),
+      home: MultiBlocProvider(
+          providers: [
+            BlocProvider<HomeScreenBloc>(
+              create: (context) => GetIt.instance<HomeScreenBloc>(),
+            ),
+          ],
           child: const HomeScreen()),
     );
   }
+}
+
+void _setupDependencies() {
+  final getIt = GetIt.instance;
+
+  getIt.registerFactory<ArticleRemoteDatasource>(() => ArticleRemoteDatasource());
+  getIt.registerFactory<ArticleTransformer>(() => ArticleTransformer());
+  getIt.registerFactory<ArticleRepository>(() => ArticleRepository(getIt(), getIt()));
+  getIt.registerFactory<FetchArticlesUseCase>(() => FetchArticlesUseCase(getIt()));
+  getIt.registerFactory<HomeScreenBloc>(() => HomeScreenBloc(getIt()));
 }
