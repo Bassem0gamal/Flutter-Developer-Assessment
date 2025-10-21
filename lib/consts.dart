b2ca52f6bfa34e591a433fb14f4ccc9c466e5408
EVO <bassemgamalosman12@gmail.com>
2025-10-21 12:28:12 +0300

    add constants for API key and placeholder image; update pubspec.yaml dependencies

---
+++ lib/consts.dart
@@ -0,0 +1,3 @@
+ const String NEWS_API_KEY = 'f5e8b0ef4ebb4dc8830a94e28a7248f4';
+
+const String PLACEHOLDER_IMAGE_URL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeJQeJyzgAzTEVqXiGe90RGBFhfp_4RcJJMQ&s';
