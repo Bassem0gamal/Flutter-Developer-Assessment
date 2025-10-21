412d45610b5f5c9f2632c3a5e01e169b5347577a
EVO <bassemgamalosman12@gmail.com>
2025-10-21 12:33:07 +0300

    implement HomeScreen with Bloc for article fetching and display; update initial app structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_developer_assessment/ui/screen/home_screen/bloc/home_screen_bloc.dart';
 import 'package:flutter_developer_assessment/ui/screen/home_screen/home_screen.dart';
 
+import 'data/api/configs_api.dart';
+
 void main() {
-  runApp(const HomeScreen());
+  runApp(const ArticleApp());
+}
+class ArticleApp extends StatelessWidget {
+  const ArticleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Article App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BlocProvider(
+          create: (context) => HomeScreenBloc(ConfigService()),
+          child: const HomeScreen()),
+    );
+  }
 }
--- lib/ui/screen/home_screen/home_screen.dart
+++ lib/ui/screen/home_screen/home_screen.dart
@@ -1,10 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_developer_assessment/consts.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'bloc/home_screen_bloc.dart';
+import 'bloc/home_screen_event.dart';
+import 'bloc/home_screen_state.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<HomeScreenBloc>().add(const FetchArticlesEvent());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: const Text('Top Headlines'),
+      ),
+      body: BlocBuilder<HomeScreenBloc, HomeScreenState>(
+        builder: (context, state) {
+          if (state is HomeScreenInitial || state is HomeScreenLoading) {
+            return const Center(child: CircularProgressIndicator());
+
+          } else if (state is HomeScreenError) {
+            return Center(child: Text('Error: ${state.message}'));
+
+          } else if (state is HomeScreenLoaded) {
+            return RefreshIndicator(
+              onRefresh: () {
+                context.read<HomeScreenBloc>().add(const OnRefreshArticlesEvent());
+                return Future.value();
+              },
+              child: ListView.builder(
+                  itemCount: state.articles.length,
+                  itemBuilder: (context, index) {
+                    final article = state.articles[index];
+                    return Card(
+                      child: ListTile(
+                        leading: Image.network(
+                          article.urlToImage ?? PLACEHOLDER_IMAGE_URL,
+                          height: 250,
+                          width: 100,
+                          fit: BoxFit.cover,
+                        ),
+                        title: Text(article.title ?? ''),
+                        subtitle: Text(article.source?.name ?? 'Unknown Source'),
+                        onTap: () {
+                          context.read<HomeScreenBloc>().add(OnTapArticleEvent(article.url ?? ''));
+                        },
+                      ),
+                    );
+                  }
+              ),
+            );
+          }
+          return const SizedBox.shrink();
+        },
+      ),
+    );
   }
 }
